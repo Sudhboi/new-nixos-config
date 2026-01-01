52d864b48fb640e75cb2c07377fe8fb4d20d6141
Sudhir Krisna <sudhboiii@gmail.com>
2026-01-01 19:34:32 -0400

    add the most useful function

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -9,9 +9,25 @@
       ".." = "cd ..";
       nrs = "sudo nixos-rebuild switch --flake ~/.dotfiles";
       hms = "home-manager switch --flake ~/.dotfiles";
+      q = "exit";
     };
     initContent = ''
+
+functions y() {
+  if [ "$1" != "" ]; then
+    if [ -d "$1" ]; then
+      yy "$1"
+    else
+      yy "$(zoxide query $1)"
+    fi
+  else
+    yy
+  fi
+    return $?
+}
+
 eval "$(zoxide init zsh)"
+
     '';
   };
 
