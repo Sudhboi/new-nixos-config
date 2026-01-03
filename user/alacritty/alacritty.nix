ad933f1321037435f747d51573cc6ae794c930da
Sudhir Krisna <sudhboiii@gmail.com>
2026-01-02 21:33:30 -0400

    improved alacritty settings

--- user/alacritty/alacritty.nix
+++ user/alacritty/alacritty.nix
@@ -5,9 +5,16 @@
     enable = true;
     settings = {
       font.normal.family = "JetBrainsMono Nerd Font";
-      window.padding = {
-	x = 10;
-	y = 10;
+      window = {
+        padding = {
+	  x = 10;
+	  y = 10;
+	};
+	opacity = 0.8;
+      };
+      cursor.style = {
+        shape = "Beam";
+	blinking = "On";
       };
     };
   };
